786a5515e60134aa4b83bd69dbe9ec9b8693ff58
Naveed Ali <advmiannaveedali@gmail.com>
2024-09-12 11:22:14 +0500

    home page updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { Flex, Heading, Stack, VStack,Text, Link, Button } from '@chakra-ui/react'
+import './Home.css'
 function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+        <div className="container">
+     <Stack
+     direction={['column','row']}
+     height="100%"
+     justifyContent={["center","space-between"]}
+     alignItems="center"
+     spacing={["16","56"]}
+     ></Stack>
+     <VStack width={"full"} alignItems={["center","flex-center"]}>
+        <Heading children="MERN STACK LEARNING"/>
+        <Text children="Find very impressive content to learn new technologies"/>
+        <Link to="/courses"/>
+         <Button size={"lg"} colorScheme='yellow'>
+         Explore Now
+            </Button>
+     </VStack>
+
+        </div>
+    </section>
   )
 }
 
