41cbd12ea18e92d03fb5b0063ddb50e22d7cefe8
Naveed Ali <advmiannaveedali@gmail.com>
2024-09-12 11:21:41 +0500

    router and routes updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Routetr,Route,Routes, Router } from 'react-router-dom';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 
 function App() {
