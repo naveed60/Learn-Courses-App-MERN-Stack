78fa1f79dbcf8024925a8f3e1b2d1ef861df96d5
Naveed Ali <advmiannaveedali@gmail.com>
2024-09-18 16:43:54 +0500

    auth routes added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Header from './components/Layout/Header/Header';
 import Courses from './components/Courses/Courses';
+import Footer from './components/Layout/Header/Footer/Footer';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import ForgetPassword from './components/Auth/ForgetPassword';
+import ResetPassword from './components/Auth/ResetPassword';
 
 function App() {
   return (<Router>
@@ -10,8 +15,12 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/courses' element={<Courses/>}/>
-
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/forgetpassword' element={<ForgetPassword/>}/>
+      <Route path='/resetpassword' element={<ResetPassword/>}/>
     </Routes>
+    <Footer/>
   </Router>
   );
 }
