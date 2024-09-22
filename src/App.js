ff6914dc3e44fbe2243babbee1937a110cdf2f60
Naveed Ali <advmiannaveedali@gmail.com>
2024-09-22 12:52:09 +0500

    auth roates  added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import ForgetPassword from './components/Auth/ForgetPassword';
 import ResetPassword from './components/Auth/ResetPassword';
+import Contact from './components/Contact /Contact';
+import About from './components/About/About';
+import Subscribe from './components/Payments/Subscribe';
+import PaymentSuccess from './components/Payments/PaymentSucces';
+import PaymentFail from './components/Payments/PaymentFail';
+import NotFound from './components/Layout/NotFound/NotFound';
 
 function App() {
   return (<Router>
@@ -15,10 +21,17 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/courses' element={<Courses/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
       <Route path='/forgetpassword' element={<ForgetPassword/>}/>
-      <Route path='/resetpassword' element={<ResetPassword/>}/>
+      <Route path='/resetpassword/:token' element={<ResetPassword/>}/>
+      <Route path='/subscribe' element={<Subscribe/>}/>
+      <Route path='*' element={<NotFound/>}/>
+      <Route path='/paymentsuccess' element={<PaymentSuccess/>}/>
+      <Route path='/paymentfail' element={<PaymentFail/>}/>
+
     </Routes>
     <Footer/>
   </Router>
